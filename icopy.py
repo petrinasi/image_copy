7a2f7c14cad5040080d226250443ef2a5a8b66f0
Petri Nasi <petri.nasi@gmail.com>
2015-09-03 09:52:14 +0300

    Put image copy in a class

--- icopy.py
+++ icopy.py
@@ -2,95 +2,111 @@
 import os
 import time
 import exifread
+from platform import system
 
-path = r"images/"
-key = "EXIF DateTimeOriginal"
-pictureFiles = ('.tiff', '.jpg')
-videoFiles = ('.avi', '.mov', '.mp4', 'mv4')
-pictureTargetRoot = r"C:\Users\nasiipet\Pictures"
-videoTargetRoot = r"C:\Users\nasiipet\Videos"
-targetFolderTemplate = "yyyy-mm "
-months = {'01': 'Tammikuu', '02': 'Helmikuu', '03': 'Maaliskuu', '04': 'Huhtikuu',
-          '05': 'Toukokuu', '06': 'KesÃ¤kuu', '07': 'HeinÃ¤kuu', '08': 'Elokuu',
-          '09': 'Syyskuu', '10': 'Lokakuu', '11': 'Marraskuu', '12': 'Joulukuu'}
+class ImageCopy:
+    key = "EXIF DateTimeOriginal"
+    picturefiles = ('.tiff', '.jpg')
+    videofiles = ('.avi', '.mov', '.mp4', '.mv4')
+    video_target_root = "Videos"
+    move_manually_folder = "move_manually"
+    months = {'01': 'Tammikuu', '02': 'Helmikuu', '03': 'Maaliskuu', '04': 'Huhtikuu',
+              '05': 'Toukokuu', '06': 'Kesäkuu', '07': 'Heinäkuu', '08': 'Elokuu',
+              '09': 'Syyskuu', '10': 'Lokakuu', '11': 'Marraskuu', '12': 'Joulukuu'}
 
+    def __init__(self, source, target, no_copy=False):
+        self.copyfrom = source
+        self.copyto = target
+        self.no_copy = no_copy
+        self.video_target_root = target + '\Videos'
 
-def getFileNamesWithFullPath(searchRoot):
-    """
-    Searches defined files starting from 'searchroot'
-    """
-    
-    for root, dirs, files in os.walk(path, topdown=True):
-        for name in files:
-            if name[name.rfind('.'):].lower() in pictureFiles:
-                print("Copying "+os.path.join(root, name))
-                source = os.path.join(root, name)
-                copyImageFile(source)
-            elif name[name.rfind('.'):].lower() in videoFiles:
-                print("Copying "+os.path.join(root, name))
-                source = os.path.join(root, name)
-                copyVideoFile(source)
-            
+    def copy_files(self):
+        """
+        Searches defined files starting from 'searchroot'
+        """
 
-def copyImageFile(source):
-    #localtime format in EXIF tag: 2012:06:23 14:26:30
-    ctime = getFileCreationTime(source)    
-    print("ctime: "+ctime)
-    if ctime == None:
-        print("File creation time missing!")
-    else:
-        dirName = ctime[:4]+"-"+ctime[5:7]+" "+months[ctime[5:7]]
-    #os.path
-    print("Directory name is: "+dirName)
-    
-def copyVideoFile(source):
-    pass
+        for root, dirs, files in os.walk(self.copyfrom, topdown=True):
+            for name in files:
+                if name[name.rfind('.'):].lower() in self.picturefiles:
+                    source = os.path.join(root, name)
+                    print("Source file:" + source)
+                    self.copy_imagefile(source)
+                elif name[name.rfind('.'):].lower() in self.videofiles:
+                    source = os.path.join(root, name)
+                    self.copy_videofile(source)
 
-def getFileCreationTime(source):
-    #localtime format in EXIF tag: 2012:06:23 14:26:30
-    ctime = getExifCreationTime(source)
-    if ctime == None:
-        """ Check file name for date i.e ddmmyyyyxxx.jpg. if it has a date use it"""
-        ctime = checkFileNameForDate(source)
-        if ctime == None:
-            ctime = getCreationTime(source)
-    return ctime
+    def copy_imagefile(self, source):
+        dir = self.get_directoryname(source)
+        print("Target directory: "+dir)
+        #os.path.
 
-def getExifCreationTime(source):    
-    f = open(source, 'rb')        
-    tags = exifread.process_file(f, details=False)
-    f.close()
-    if key in tags:
-        return str(tags["EXIF DateTimeOriginal"])        
-    else:        
-        return None
+    def copy_videofile(self, source):
+        pass
 
-def getCreationTime(source):
-    """ Return local time format:
-            time.struct_time(tm_year=2013, tm_mon=5, tm_mday=22, tm_hour=17,
-            tm_min=43, tm_sec=0, tm_wday=2, tm_yday=142, tm_isdst=1)
-    """
-    ltime = time.localtime(os.path.getctime(source))
-    ctime = time.strftime("%Y:%m", ltime)
-    print("File timestamp"+ctime)
-    return str(ctime)
-               
-def checkFileNameForDate(source):
-    print "Source: "+source
-    fileName = source.replace("\\","/")    
-    fileName = fileName[fileName.rfind("/")+1:]
-    print "Filename: "+fileName  
-    # if file name has time format as ddmmyyyy
-    if fileName.isdigit() and \
-        int(fileName[:2]) > 0 and int(fileName[:2]) <= 31 and \
-        int(fileName[1:3]) > 0 and int(fileName[1:3]) <= 12 and \
-        int(fileName[3:7]) >= 1999 and int(fileName[3:7]) < 2020:
-        
-        return str(filename[3:7]+":"+filename[:1])
-    else:
-        return None
-        
-getFileNamesWithFullPath(path)
+    def get_directoryname(self, source):
+        # localtime format in EXIF tag: 2012:06:23 14:26:30
+        ctime = self.get_file_creationtime(source)
+        print("ctime: " + ctime)
+        if ctime is None:
+            print("File creation time missing! File moved to "+ self.move_manually_folder)
+            dirname = self.move_manually_folder
+        else:
+            dirname = ctime[:4] + "-" + ctime[5:7] + " " + self.months[ctime[5:7]]
 
+        print("Target directory name is: " + dirname)
+        return dirname
 
+    def get_file_creationtime(self, source):
+        # localtime format in EXIF tag: 2012:06:23 14:26:30
+        ctime = self.get_exif_creationtime(source)
+        if ctime is None:
+            """ Check file name for date i.e ddmmyyyyxxx.jpg. if it has a date use it"""
+            ctime = self.get_filenamedate(source)
+            if ctime is None:
+                ctime = self.get_creationtime(source)
+        return ctime
 
+    def get_exif_creationtime(self, source):
+        f = open(source, 'rb')
+        tags = exifread.process_file(f, details=False, stop_tag=self.key)
+        f.close()
+        if self.key in tags:
+            return str(tags[self.key])
+        else:
+            return None
+
+    def get_creationtime(self, source):
+        """ Return local time format:
+                time.struct_time(tm_year=2013, tm_mon=5, tm_mday=22, tm_hour=17,
+                tm_min=43, tm_sec=0, tm_wday=2, tm_yday=142, tm_isdst=1)
+        """
+
+        if system() is 'Windows':
+            #On Windows use getctime()
+            ltime = time.localtime(os.path.getctime(source))
+        else:
+            #On POSIX system use getmtime()
+            ltime = time.localtime(os.path.getmtime(source))
+
+        ctime = time.strftime("%Y:%m", ltime)
+
+        print("File timestamp" + ctime)
+        return str(ctime)
+
+    def get_filenamedate(self, source):
+        print "Source: " + source
+        filename = source.replace("\\", "/")
+        filename = filename[filename.rfind("/") + 1:]
+        print "filename: " + filename
+        # if file name has time format as ddmmyyyy
+        if filename.isdigit() and \
+                        int(filename[:2]) <= 31 and \
+                                0 < int(filename[1:3]) <= 12 and \
+                                1999 <= int(filename[3:7]) < 2030:
+
+            return str(filename[3:7] + ":" + filename[:1])
+        else:
+            return None
+
+test = ImageCopy("images", "copyed_imges")
+test.copy_files()
