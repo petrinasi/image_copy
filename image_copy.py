fa16a715423d508b438cc4e5df2c6943a9351df2
Petri Nasi <petri.nasi@gmail.com>
2015-09-03 22:39:11 +0300

    Fixed path problem when path had white spaces

---
+++ image_copy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+from icopy import ImageCopy
+
+def main(args):
+    for arg in args:
+        print arg
+    ic = ImageCopy(args[0], args[1])
+    ic.copy_files()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
